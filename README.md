459c3f63473d9acf37f8facf05278e10f351131d
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-04-02 22:42:36 -0500

    adding updated readme and script.js

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # PasswordGenerator
+I have created a password generator using javascript. 
+
+I started by creating generate password button to generate a password when clicked.
+I then identified variables:lowercase, uppercase, numeric, and special characters.
+I then initialized empty string for password.
+
+I then wrote the user prompts for desired password lengths and character types.
+I also created alerts to appear if the password criteria isn't met.
+I then created a length variable and ran a "for loop" to iterate length number of times and run the "generate function" character in it. 
+Then I return the resulting string. 
+Finally I added an event listener to the generate button.
+
+My password generator creates random passwords for users.
+
+https://github.com/Karina1023/PasswordGenerator.git
+
+
+https://karina1023.github.io/PasswordGenerator/
--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-// Assignment Code
-//now generate btn is now a reference to the generate password button
 var generateBtn = document.querySelector("#generate"); 
-//lowercase, uppercase,numeric, and special characters
+//variables identified
 var lowChar = "abcdefghijklmnopqrstuvwxyz";
 var upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
 var special = "~!@#$%^&*()_-=;:?<>[]\.,|{}";
 var selectionString = "";
 //Step 1: look up how to generate a random character password
-//function generatePassword(){
-  //var length = 8;
   function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
   
-  
-  
-  
-// Write password to the #password input
 function generatePassword() {  
 
-//
 
   var selectionString = "";
   var password = "";
@@ -75,35 +66,5 @@ var charactersLength = selectionString.length;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); //when generate button is clicked then call writepassword function
-
-
-  //your task is to generate a string with the required criteria
-  //return "We are doing a pseudocode session for the third homework assignment"
-
-
-//Hints
-//google how to generate a random character
-//function getRandom(){}
-
-//console.log(Math.floor (Math.random()*3) +
-// Step 2: Say we now have a LENGTH variable, then run a for loop and it should iterate LENGTH number of times and run the generate character function in it,
-  //Step 3: return the resulting string
-
-// use a for loop to generate N number of random characters.
-//look at html to get an idea of whats going on and what its talking about but only manipulate and change script.js
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
+generateBtn.addEventListener("click", writePassword); 
+//when generate button is clicked then call writepassword function
